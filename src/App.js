a1c3c60a2d4ee14eb9262db63c8591264ee51fa3
dm1tro69 <dimolg22@gmail.com>
2022-04-02 15:27:43 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
-import './categories.styles.scss'
-import CategoryItem from "./componets/category-item/CategoryItem";
+import React from 'react';
+import Home from "./routes/home/Home";
+import {Route, Routes} from "react-router-dom";
+import Navigation from "./routes/navigation/Navigation";
+
+
 
 const App = () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<Navigation/>}>
+                <Route index element={<Home/>}/>
+            </Route>
 
-    const categories = [
-        {
-            "id": 1,
-            "title": "hats",
-            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },
-        {
-            "id": 2,
-            "title": "jackets",
-            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-        },
-        {
-            "id": 3,
-            "title": "sneakers",
-            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-        },
-        {
-            "id": 4,
-            "title": "womens",
-            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },
-        {
-            "id": 5,
-            "title": "mens",
-            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-    ]
-
-  return (
-    <div className={'categories-container'}>
-        {categories.map((category) => (
-            <CategoryItem key={category.id} category={category}/>
-        ))}
-
-    </div>
-
-
-  )
-}
+        </Routes>
+    );
+};
 
 export default App;
